bcf9d430381506ddbbd745e83d5020ace4d2a877
Ancientshield <jacklin2100@gmail.com>
2024-07-06 14:45:47 +0800

    deep ref

--- src/componenets/Person.vue
+++ src/componenets/Person.vue
@@ -9,42 +9,29 @@
 			<li v-for="g in games" v-bind:key="g.id">{{ g.name }}</li>
 		</ul>
 		<button @click="changeFirstGame">修改第一個遊戲的名字</button>
-		<hr />
-		<h2>測試：{{ obj.a.b.c }}</h2>
-		<button @click="changeObj">修改</button>
 	</div>
 </template>
 
 <script lang="ts" setup name="Person">
-	import { reactive } from 'vue';
+	import { ref } from 'vue';
 
 	// 物件、陣列 類型用 reactive 會變成 Proxy
-	let car = reactive({ brand: '賓士', price: 100 });
+	let car = ref({ brand: '賓士', price: 100 });
 	const changePrice = () => {
-		car.price += 1;
+		car.value.price += 1;
 	};
 
 	// 物件、陣列 類型用 reactive 會變成 Proxy
-	let games = reactive([
+	let games = ref([
 		{ id: '001', name: '王者榮耀' },
 		{ id: '002', name: '原神' },
 		{ id: '003', name: '三國志' },
 	]);
+	// 陣列要在前面 .value
 	const changeFirstGame = () => {
-		games[0].name = '流星蝴蝶劍';
+		games.value[0].name = '流星蝴蝶劍';
 	};
-
-	// reactive 深層次修改
-	let obj = reactive({
-		a: {
-			b: {
-				c: 666,
-			},
-		},
-	});
-	const changeObj = () => {
-		obj.a.b.c = 999;
-	};
+	// 結論：ref 遇到物件or陣列，底層還是用 reactive
 </script>
 
 <style scoped>
