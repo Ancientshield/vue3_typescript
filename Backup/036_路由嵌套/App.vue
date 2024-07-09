a44bb1ab79fd1270066de774719049f2f33659d3
Ancientshield <jacklin2100@gmail.com>
2024-07-09 21:22:02 +0800

    路由嵌套

---
+++ Backup/036_路由嵌套/App.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="app">
+		<h2 class="title">Vue路由測試</h2>
+		<!-- 導航區靠路由 rander 出來的就是路由元件，放在views資料夾內 -->
+		<div class="navigate">
+			<RouterLink to="/home" active-class="active">首頁</RouterLink>
+			<RouterLink :to="{ name: '新聞' }" active-class="active">新聞</RouterLink>
+			<RouterLink :to="{ path: '/about' }" active-class="active"
+				>關於</RouterLink
+			>
+		</div>
+		<!-- 展示區 -->
+		<div class="main-content">
+			<RouterView></RouterView>
+		</div>
+	</div>
+</template>
+<script lang="ts" setup name="App">
+	import { RouterView, RouterLink } from 'vue-router';
+	import Header from './components/Header.vue';
+</script>
+
+<style scoped>
+	/* App */
+	.title {
+		text-align: center;
+		word-spacing: 5px;
+		margin: 30px 0;
+		height: 70px;
+		line-height: 70px;
+		background-image: linear-gradient(45deg, gray, white);
+		border-radius: 10px;
+		box-shadow: 0 0 2px;
+		font-size: 30px;
+	}
+	.navigate {
+		display: flex;
+		justify-content: space-around;
+		margin: 0 100px;
+	}
+	.navigate a {
+		display: block;
+		text-align: center;
+		width: 90px;
+		height: 40px;
+		line-height: 40px;
+		border-radius: 10px;
+		background-color: gray;
+		text-decoration: none;
+		color: white;
+		font-size: 18px;
+		letter-spacing: 5px;
+	}
+	.navigate a.active {
+		background-color: #64967e;
+		color: #ffc268;
+		font-weight: 900;
+		text-shadow: 0 0 1px black;
+		font-family: 微软雅黑;
+	}
+	.main-content {
+		margin: 0 auto;
+		margin-top: 30px;
+		border-radius: 10px;
+		width: 90%;
+		height: 400px;
+		border: 1px solid;
+	}
+</style>
---
+++ Backup/036_路由嵌套/views/News.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="news">
+		<!-- 導航區 -->
+		<ul>
+			<li v-for="news in newsList" :key="news.id">
+				<RouterLink :to="{ path: '/news/detail' }">{{ news.title }}</RouterLink>
+			</li>
+		</ul>
+		<!-- 展示區 -->
+		<div class="news-content"><RouterView></RouterView></div>
+	</div>
+</template>
+
+<script setup lang="ts" name="News">
+	import { reactive } from 'vue';
+
+	let newsList = reactive([
+		{ id: '001', title: '一種抗癌食物', content: '西蘭花' },
+		{ id: '002', title: '如何一夜致富', content: '學IT' },
+		{ id: '003', title: '震驚！萬萬沒想到', content: '明天是週一' },
+		{ id: '004', title: '好消息！好消息！', content: '大樂透頭彩獎金十億' },
+	]);
+</script>
+
+<style scoped>
+	.news {
+		padding: 0 20px;
+		display: flex;
+		justify-content: space-between;
+		height: 100%;
+	}
+	.news ul {
+		margin-top: 30px;
+		list-style: none;
+		padding-left: 10px;
+	}
+	.news li > a {
+		font-size: 18px;
+		line-height: 40px;
+		text-decoration: none;
+		color: #64967e;
+		text-shadow: 0 0 1px rgb(0, 84, 0);
+	}
+	.news-content {
+		width: 70%;
+		height: 90%;
+		border: 1px solid;
+		margin-top: 20px;
+		border-radius: 10px;
+	}
+</style>
