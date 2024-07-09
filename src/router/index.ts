8517f1fde55dca0cab72c3fad6fc5bb060176542
Ancientshield <jacklin2100@gmail.com>
2024-07-09 17:52:45 +0800

    use RouterView and RouterLink

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+// 引入 createRouter
+import { createRouter, createWebHistory } from 'vue-router';
+
+// 引入元件
+import Home from '@/components/Home.vue';
+import About from '@/components/About.vue';
+import News from '@/components/News.vue';
+
+// 創建路由器
+const router = createRouter({
+	history: createWebHistory(), // 路由器工作模式
+	routes: [
+		// 每一個路由的工作規則
+		{
+			path: '/home',
+			component: Home,
+		},
+		{
+			path: '/about',
+			component: About,
+		},
+		{
+			path: '/news',
+			component: News,
+		},
+	],
+});
+
+export default router;
