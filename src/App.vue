6fe202fc01b9737c2061f513dbfa7394afed9dfc
Ancientshield <jacklin2100@gmail.com>
2024-07-12 14:18:37 +0800

    $atter 祖傳孫&孫傳祖

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="container-fluid wraper">
-		<h1 class="title">Vue3 组件间通信</h1>
+		<h1 class="title">Vue3 元件間通信</h1>
 		<hr />
 		<div class="row">
 			<div class="col-xs-3 col-md-3 col-lg-3 col-xl-3">
-				<!-- 导航区 -->
+				<!-- 導航區 -->
 				<router-link active-class="active" class="list-group-item" to="/props"
 					>1. props</router-link
 				>
 				<router-link active-class="active" class="list-group-item" to="/event"
-					>2. 自定义事件</router-link
+					>2. 自定義事件</router-link
 				>
 				<router-link active-class="active" class="list-group-item" to="/mitt"
 					>3. mitt</router-link
@@ -41,7 +41,7 @@
 			</div>
 			<div class="col-xs-9 col-md-9 col-lg-9 col-xl-9">
 				<div class="panel-body">
-					<!-- 占位一个展示区 -->
+					<!-- 展示區 -->
 					<router-view></router-view>
 				</div>
 			</div>
--- src/pages/05_$attrs/Child.vue
+++ src/pages/05_$attrs/Child.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="child">
-		<h3>子组件</h3>
-		<GrandChild v-bind="$attrs"/>
+		<h3>子元件</h3>
+		<GrandChild v-bind="$attrs" />
 	</div>
 </template>
 
 <script setup lang="ts" name="Child">
-	import GrandChild from './GrandChild.vue'
+	import GrandChild from './GrandChild.vue';
 </script>
 
 <style scoped>
-	.child{
+	.child {
 		margin-top: 20px;
 		background-color: skyblue;
 		padding: 20px;
