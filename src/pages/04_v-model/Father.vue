ec05c710d04bd81bc8f534ce5038a5cb6993e7e8
Ancientshield <jacklin2100@gmail.com>
2024-07-12 11:29:20 +0800

    v-model 原生html和Vue標籤的$event差異

--- src/pages/04_v-model/Father.vue
+++ src/pages/04_v-model/Father.vue
@@ -1,36 +1,35 @@
 <template>
-  <div class="father">
-    <h3>父组件</h3>
-    <h4>{{ username }}</h4>
-    <h4>{{ password }}</h4>
-    <!-- v-model用在html标签上 -->
-    <!-- <input type="text" v-model="username"> -->
-    <!-- <input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
+	<div class="father">
+		<h3>父组件</h3>
+		<!-- <h4>{{ other }}</h4> -->
+		<h4>{{ username }}</h4>
+		<h4>{{ password }}</h4>
+		<!-- v-model用在html标签上 -->
+		<!-- <input type="text" v-model="username"> -->
+		<!-- <input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
 
-    <!-- v-model用在组件标签上 -->
-    <!-- <AtguiguInput v-model="username"/> -->
-    <!-- <AtguiguInput 
-      :modelValue="username" 
-      @update:modelValue="username = $event"
-    /> -->
+		<!-- v-model用在组件标签上 -->
+		<!-- <AtguiguInput v-model="other"/> -->
+		<!-- <AtguiguInput :modelValue="other" @update:modelValue="other = $event" /> -->
 
-    <!-- 修改modelValue -->
-    <AtguiguInput v-model:ming="username" v-model:mima="password"/>
-  </div>
+		<!-- 修改 modelValue -->
+		<AtguiguInput v-model:ming="username" v-model:mima="password" />
+	</div>
 </template>
 
 <script setup lang="ts" name="Father">
-	import { ref } from "vue";
-  import AtguiguInput from './AtguiguInput.vue'
-  // 数据
-  let username = ref('zhansgan')
-  let password = ref('123456')
+	import { ref } from 'vue';
+	import AtguiguInput from './AtguiguInput.vue';
+
+	// let other = ref('other');
+	let username = ref('zhansgan');
+	let password = ref('123456');
 </script>
 
 <style scoped>
-.father {
-  padding: 20px;
-  background-color: rgb(165, 164, 164);
-  border-radius: 10px;
-}
+	.father {
+		padding: 20px;
+		background-color: rgb(165, 164, 164);
+		border-radius: 10px;
+	}
 </style>
