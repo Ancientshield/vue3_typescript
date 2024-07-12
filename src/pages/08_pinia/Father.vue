b7112aa25bae6e4a880a7d8a976e6cd638c91a11
Ancientshield <jacklin2100@gmail.com>
2024-07-12 15:47:07 +0800

    skip pinia

--- src/pages/08_pinia/Father.vue
+++ src/pages/08_pinia/Father.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="father">
-    <h3>父组件</h3>
-    <h3>直接参考之前所讲的pinia即可</h3>
-  </div>
+	<div class="father">
+		<h3>父元件</h3>
+		<h3>直接參考之前寫的pinia即可</h3>
+	</div>
 </template>
 
-<script setup lang="ts" name="Father">
-</script>
+<script setup lang="ts" name="Father"></script>
 
 <style scoped>
-  .father{
-    background-color: rgb(165, 165, 165);
-    padding: 20px;
-    border-radius: 10px;
-  }
+	.father {
+		background-color: rgb(165, 165, 165);
+		padding: 20px;
+		border-radius: 10px;
+	}
 </style>
