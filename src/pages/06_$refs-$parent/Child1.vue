62aaff018fc6fa4d01be74135ee4389da09081a6
Ancientshield <jacklin2100@gmail.com>
2024-07-12 15:20:38 +0800

    $ref $parent defineExpose

--- src/pages/06_$refs-$parent/Child1.vue
+++ src/pages/06_$refs-$parent/Child1.vue
@@ -1,34 +1,32 @@
 <template>
-  <div class="child1">
-    <h3>子组件1</h3>
+	<div class="child1">
+		<h3>子元件1</h3>
 		<h4>玩具：{{ toy }}</h4>
-		<h4>书籍：{{ book }} 本</h4>
-		<button @click="minusHouse($parent)">干掉父亲的一套房产</button>
-  </div>
+		<h4>書籍：{{ book }} 本</h4>
+		<button @click="minusHouse($parent)">賣掉父親一棟房產</button>
+	</div>
 </template>
 
 <script setup lang="ts" name="Child1">
-	import { ref } from "vue";
-	// 数据
-	let toy = ref('奥特曼')
-	let book = ref(3)
+	import { ref } from 'vue';
 
-	// 方法
-	function minusHouse(parent:any){
-		parent.house -= 1
-	}
+	let toy = ref('奥特曼');
+	let book = ref(3);
 
-	// 把数据交给外部
-	defineExpose({toy,book})
+	const minusHouse = (parent: any) => {
+		if (parent.house > 0) parent.house -= 1;
+	};
 
+	// 向外部提供數據
+	defineExpose({ toy, book });
 </script>
 
 <style scoped>
-	.child1{
+	.child1 {
 		margin-top: 20px;
 		background-color: skyblue;
 		padding: 20px;
 		border-radius: 10px;
-    box-shadow: 0 0 10px black;
+		box-shadow: 0 0 10px black;
 	}
 </style>
